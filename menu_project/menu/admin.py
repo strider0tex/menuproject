7ddea443c90b20eca083e9649db71349ff288183
igor <>
2024-02-18 14:19:26 +0300

    remove files

--- menu_project/menu/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem
-
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent', 'url', 'named_url')
-    list_filter = ('parent',)
--- menu_project/menu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from menu.views import MenuView
-
-#comment
-
-urlpatterns = [
-    path('menu/', MenuView.as_view(), name='menu'),
-]
